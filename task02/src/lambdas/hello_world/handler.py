c8b53ea3067737e03f90faf04caade202ecd89f1
Dawid Bozek <dawid_bozek@epam.com>
2025-03-28 05:32:02 +0100

    .

--- task02/src/lambdas/hello_world/handler.py
+++ task02/src/lambdas/hello_world/handler.py
@@ -1,6 +1,7 @@
 from commons.log_helper import get_logger
 from commons.abstract_lambda import AbstractLambda
 
+import json
 _LOG = get_logger(__name__)
 
 
@@ -10,16 +11,30 @@ class HelloWorld(AbstractLambda):
         pass
         
     def handle_request(self, event, context):
-        """
-        Explain incoming event here
-        """
-        # todo implement business logic
-        response = {
-            "statusCode": 200,
-            "message": "Hello from Lambda"
-        }
-        return response
-    
+
+        request_info = event.get('requestContext', {}).get('http', {})
+        method = request_info.get('method', 'UNKNOWN')
+        path = request_info.get('path', '')
+
+        if method == 'GET' and path == '/hello':
+            response_body = {
+                "statusCode": 200,
+                "message": "Hello from Lambda"
+            }
+            return {
+                "statusCode": 200,
+                "body": json.dumps(response_body)
+            }
+        else:
+            error_message = {
+                "statusCode": 400,
+                "message": f"Invalid request. Path: {path}, Method: {method}"
+            }
+            return {
+                "statusCode": 400,
+                "body": json.dumps(error_message)
+            }
+
 
 HANDLER = HelloWorld()
 
