d487845e33ed39b611b651e9a7268b9a82ce89f2
Dawid Bozek <dawid_bozek@epam.com>
2025-03-28 05:06:18 +0100

    .

--- task01/tests/test_hello_world/test_success.py
+++ task01/tests/test_hello_world/test_success.py
@@ -4,5 +4,5 @@ from tests.test_hello_world import HelloWorldLambdaTestCase
 class TestSuccess(HelloWorldLambdaTestCase):
 
     def test_success(self):
-        self.assertEqual(self.HANDLER.handle_request(dict(), dict()), 200)
+        self.assertEqual(self.HANDLER.handle_request(dict(), dict()), {'statusCode': 200, 'message': 'Hello from Lambda'})
 
