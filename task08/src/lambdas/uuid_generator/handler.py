922c31f9dcae194602797e8481b15aaa0c556b58
Dawid Bozek <dawid_bozek@epam.com>
2025-04-02 02:07:53 +0200

    .

--- task08/src/lambdas/uuid_generator/handler.py
+++ task08/src/lambdas/uuid_generator/handler.py
@@ -19,18 +19,22 @@ class UuidGenerator(AbstractLambda):
         """
         Generate and pass UUIDs
         """
+        try:
+            unique_ids = [str(uuid.uuid4()) for i in range(10)]
+            j_dump = json.dumps({
+                'ids': unique_ids})
 
-        unique_ids = [str(uuid.uuid4()) for i in range(10)]
-        j_dump = json.dumps({
-            'ids': unique_ids})
-
-        name = datetime.now(timezone.utc).isoformat(timespec="milliseconds").replace("+00:00", "Z")
-        boto3.client('s3').put_object(
-            Bucket=os.getenv('target_bucket', 'uuid-storage'),
-            Key= f"name",
-            Body=j_dump
-        )
-        _LOG.info(f"Success, ")
+            name = datetime.now(timezone.utc).isoformat(timespec="milliseconds").replace("+00:00", "Z")
+            boto3.client('s3').put_object(
+                Bucket=os.getenv('target_bucket', 'uuid-storage'),
+                Key= f"name",
+                Body=j_dump,
+                ContentType='application/json'
+            )
+            _LOG.info(f"Success {name}")
+        except Exception as e:
+            _LOG.error(f'Error:{e}')
+            raise e
 
     
 
