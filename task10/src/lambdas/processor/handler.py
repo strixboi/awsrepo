43d209b011392fe8e18d8b8d7451707cea817361
Dawid Bozek <dawid_bozek@epam.com>
2025-04-02 08:29:50 +0200

    .

--- task10/src/lambdas/processor/handler.py
+++ task10/src/lambdas/processor/handler.py
@@ -29,26 +29,24 @@ class Processor(AbstractLambda):
             request_info = request.json()
             _LOG.info("Getting to creation")
 
-            # record = {'id': str(uuid.uuid4()),
-            #         'forecast': {
-            #     "elevation": request_info['elevation'],
-            #     "generationtime_ms": request_info["generationtime_ms"],
-            #     "hourly": {
-            #         "temperature_2m": request_info['hourly']['temperature_2m'],
-            #         "time": request_info['hourly']['time'],
-            #     },
-            #     "hourly_units": {
-            #         "temperature_2m": str(request_info['hourly_units']['temperature_2m']),
-            #         "time": str(request_info['hourly_units']['time']),
-            #     },
-            #     "latitude": request_info["latitude"],
-            #     "longitude": request_info["longitude"],
-            #     "timezone": request_info["timezone"],
-            #     "timezone_abbreviation": request_info["timezone_abbreviation"],
-            #     "utc_offset_seconds": request_info["utc_offset_seconds"]
-            # }}
-
-            record = {{"id": "1b472527-d5d1-4aea-84c7-328a508d3cb5", "forecast": {"latitude": 50.4375, "longitude": 30.5, "generationtime_ms": 0.025033950805664062, "utc_offset_seconds": 7200, "timezone": "Europe/Kiev", "timezone_abbreviation": "EET", "elevation": 188.0, "hourly_units": {"time": "iso8601", "temperature_2m": "°C"}, "hourly": {"time": ["2023-12-04T00:00", "2023-12-04T01:00", "2023-12-04T02:00", "..."], "temperature_2m": [-2.4, -2.8, -3.2, "..."]}}}}
+            record = {'id': str(uuid.uuid4()),
+                    'forecast': {
+                "elevation": request_info['elevation'],
+                "generationtime_ms": request_info["generationtime_ms"],
+                "hourly": {
+                    "temperature_2m": request_info['hourly']['temperature_2m'],
+                    "time": request_info['hourly']['time'],
+                },
+                "hourly_units": {
+                    "temperature_2m": str(request_info['hourly_units']['temperature_2m']),
+                    "time": str(request_info['hourly_units']['time']),
+                },
+                "latitude": request_info["latitude"],
+                "longitude": request_info["longitude"],
+                "timezone": request_info["timezone"],
+                "timezone_abbreviation": request_info["timezone_abbreviation"],
+                "utc_offset_seconds": request_info["utc_offset_seconds"]
+            }}
 
             item = json.loads(json.dumps(record), parse_float=Decimal)
             table.put_item(Item = item)
